71190f4d591dacdb9508c83aac94394ff2c18a3d
swallehrashid <swallehmtawazo@gmail.com>
2025-09-08 06:41:40 +0300

    adds an event to the buttons in the home page

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+// JS/index.js
+
+  document.getElementById("nav-bar-btn").addEventListener("click", function() {
+      window.location.href = "contact.html";  
+  });
+
+   document.getElementById("hero-section-btn").addEventListener("click", function() {
+      window.location.href = "projects.html";  
+  });
+
+   document.getElementById("about-section-btn").addEventListener("click", function() {
+      window.location.href = "about.html";  
+  });
+
+  document.getElementById("projects-section-btn").addEventListener("click", function() {
+      window.location.href = "projects.html";  
+  });
+
+  document.getElementById("call-to-action-section-btn").addEventListener("click", function() {
+      window.location.href = "contact.html";  
+  });
+
+
